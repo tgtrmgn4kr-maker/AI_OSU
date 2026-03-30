fa040847495b410729994516b37f3d51a970977e
tgtrmgn4kr-maker <tgtrmgn4kr@privaterelay.appleid.com>
2026-03-30 20:41:05 +0800

    The first commit

---
+++ CSharpDXGICapture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpDXGICapture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
